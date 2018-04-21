1af27106ae75ddb32e9f19f279134a7c4297f74b
Piotr <piruci88@gmail.com>
2018-04-21 22:29:25 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/Noise.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Noise : MonoBehaviour
+{
+    float TimeM;
+    public GameObject Anim;
+
+    void Start()
+    {
+        TimeM = 0f;
+    }
+
+    void FixedUpdate()
+    {
+        TimeM += Time.deltaTime;
+
+        if (TimeM> 9.0f )
+            Anim.SetActive(true);
+        else
+            Anim.SetActive(false);
+        if (TimeM > 10.0f)
+            TimeM = 0;
+
+    }
+
+}
