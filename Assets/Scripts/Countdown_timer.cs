9e2aea1c9b0dfa6694c33768e15ab43cb1b51ae1
Piotr <piruci88@gmail.com>
2018-04-21 17:07:07 +0200

    Auto stash before merge of "master" and "origin/master"

--- Assets/Scripts/Countdown_timer.cs
+++ Assets/Scripts/Countdown_timer.cs
@@ -8,13 +8,26 @@ public class Countdown_timer : MonoBehaviour {
     public float MainGameTimer = 99;
     public Text timerText;
 
-	// Use this for initialization
-	void Start () {
+    public GameObject GameOver;
+
+    private int FramesCount;
+
+    // Use this for initialization
+    void Start () {
         timerText = GetComponent<Text>();
+        FramesCount = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        //if(MainGameTimer>=50)
+        
+        //else if(MainGameTimer>=50)
+
+        if(MainGameTimer<=0)
+            GameOver.SetActive(true);
+
         MainGameTimer -= Time.deltaTime;
         timerText.text = MainGameTimer.ToString("f0");
 	}
