11c323b08a5e44856d0710e4b1d0b94a3839a134
Mauser <filip1209348756@gmail.com>
2018-04-21 22:21:28 +0200

    Dodane pierwsze dźwięki

--- Assets/Scripts/Countdown_timer.cs
+++ Assets/Scripts/Countdown_timer.cs
@@ -8,40 +8,14 @@ public class Countdown_timer : MonoBehaviour {
     public float MainGameTimer = 99;
     public Text timerText;
 
-    public GameObject GameOver;
-
-    private float Mult;
-
-    private int FramesCount;
-
-    // Use this for initialization
-    void Start () {
+	// Use this for initialization
+	void Start () {
         timerText = GetComponent<Text>();
-        FramesCount = 0;
-        Mult = 1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-        //if(MainGameTimer>=50)
-        
-        //else if(MainGameTimer>=50)
-
-        if(MainGameTimer<=0)
-            GameOver.SetActive(true);
-
-        MainGameTimer -= Mult*Time.deltaTime;
+        MainGameTimer -= Time.deltaTime;
         timerText.text = MainGameTimer.ToString("f0");
 	}
-
-    public void SetTimeMultiplication(float Multiplicator)
-    {
-        Mult = Multiplicator;
-    }
-
-    void LateUpdate()
-    {
-        
-    }
 }
--- Assets/Scripts/NextLev.cs
+++ Assets/Scripts/NextLev.cs
@@ -9,28 +9,15 @@ public class NextLev : MonoBehaviour {
 
     public GameObject NextLevName;
     public GameObject ThisLevName;
-
-    public bool Dodatnie;
-
-    private GameObject Time;
-
-    void Start()
-    {
-        Time = GameObject.Find("Canvas");
-    }
+    //private GameObject 
 
     void OnMouseDown()
     {
 
-        ThisLevName.SetActive(false);
         NextLevName.SetActive(true);
-
-        if (Dodatnie)
-            Time.GetComponentInChildren<Countdown_timer>().MainGameTimer += 10;
-        else
-            Time.GetComponentInChildren<Countdown_timer>().MainGameTimer -= 15;
+        ThisLevName.SetActive(false);
+        
     }
-
 }
 
 /*
