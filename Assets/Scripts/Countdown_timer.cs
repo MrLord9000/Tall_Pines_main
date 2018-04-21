3033a3fd1dbbe5bd8b277f9801411a8870481c5c
Unknown <filip1209348756@gmail.com>
2018-04-21 14:45:12 +0200

    Repo Init

---
+++ Assets/Scripts/Countdown_timer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Countdown_timer : MonoBehaviour {
+
+    public float MainGameTimer = 99;
+    public Text timerText;
+
+	// Use this for initialization
+	void Start () {
+        timerText = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MainGameTimer -= Time.deltaTime;
+        timerText.text = MainGameTimer.ToString("f0");
+	}
+}
---
+++ Assets/Scripts/NextLev.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class NextLev : MonoBehaviour {
+
+    public GameObject NextLevName;
+    public GameObject ThisLevName;
+    //private GameObject 
+
+    void OnMouseDown()
+    {
+
+        NextLevName.SetActive(true);
+        ThisLevName.SetActive(false);
+        
+    }
+}
+
+/*
+public class NextLev : MonoBehaviour
+{
+    public Button yourButton;
+
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+
+        Debug.Log("dziala");
+    }
+}
+*/
