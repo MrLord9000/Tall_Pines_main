802d6b944b3530aae200a6158976107653cdf05c
Piotr <piruci88@gmail.com>
2018-04-21 16:43:40 +0200

    Game Over

--- Assets/Scripts/Countdown_timer.cs
+++ Assets/Scripts/Countdown_timer.cs
@@ -6,17 +6,22 @@ using UnityEngine.UI;
 
 public class Countdown_timer : MonoBehaviour {
 
-    public float MainGameTimer = 99;
+    public float MainGameTimer = 30;
     public Text timerText;
 
-	// Use this for initialization
-	void Start () {
+    public GameObject GameOver;
+
+
+    // Use this for initialization
+    void Start () {
         timerText = GetComponent<Text>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+        if(MainGameTimer<=0)
+            GameOver.SetActive(true);
 
         MainGameTimer -= Time.deltaTime;
         timerText.text = MainGameTimer.ToString("f0");
