0227f92200f8fb0e1919cc2e887e13da0418c383
Unknown <qswdw3ed3@gmail.com>
2018-04-21 17:16:19 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/ObjectsObtainingController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectsObtainingController : MonoBehaviour {
+
+    public GameObject ObjectsSystem;
+    public int ObjectNumber = 1;
+
+    private int flashlight = 1;
+    private int object2 = 2;
+
+	// Use this for initialization
+	void Start ()
+    {
+        ObjectsSystem = GameObject.Find("GameObjectsController");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void OnMouseDown()
+    {
+        ObjectsSystem.GetComponent<ObjectsSytemController>().GotObject( ObjectNumber );
+        this.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/ObjectsSytemController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectsSytemController : MonoBehaviour {
+
+    public GameObject Object1;
+    public float FlashlightTime = 2.0f;
+    private float timeElapsed = 0.0f;
+    public GameObject Object2;
+
+    public bool BOOLObject1 = false;
+    public bool BOOLObject2 = false;
+
+    // Use this for initialization
+    void Start ()
+    {
+        BOOLObject1 = false;
+        BOOLObject2 = false;
+
+        Object1.SetActive(false);
+        Object2.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if ( BOOLObject1 )
+        {
+            if ( Time.time - timeElapsed >= FlashlightTime )
+            {
+                Debug.Log("zgasła.");
+                Object1.SetActive(false);
+            }
+        }
+    }
+
+    public void GotObject( int number )
+    {
+        if ( number == 1 )
+        {
+            BOOLObject1 = true;
+            Debug.Log("masz przedmiot.latarka świeci");
+            Object1.SetActive(true);
+            timeElapsed = Time.time;
+        }
+    }
+}
