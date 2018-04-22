2155f0a2d4d0a5c6a4b78becfd455f9020f98d3c
Mauser <filip1209348756@gmail.com>
2018-04-22 11:37:06 +0200

    Intro i gamover in progress

---
+++ Assets/Scripts/GameOver_restart.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver_restart : MonoBehaviour {
+
+
+	void OnMouseDown () {
+        SceneManager.LoadScene("Piotrek");
+    }
+}
