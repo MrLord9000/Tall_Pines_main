3f4110c27cfcf272998d4f13deb66e1dff0e35f0
Piotr <piruci88@gmail.com>
2018-04-22 12:55:06 +0200

    Sceny 20+

---
+++ Assets/Scripts/BlackScreen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackScreen : MonoBehaviour {
+
+    GameObject Black;
+
+	// Use this for initialization
+	void Start () {
+        Black = GameObject.Find("Czarny");
+	}
+
+    // Update is called once per frame
+
+    void OnMouseDown()
+    {
+        Black.SetActive(true);
+        Black.SetActive(false);
+    }
+}
